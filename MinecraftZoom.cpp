c2048b6d9a1d01d10c800c8e1628ee2d432f19c6
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-27 17:08:13 -0400

    Added check at startup to see if Minecraft Bedrock Edition is open

--- MinecraftZoom.cpp
+++ MinecraftZoom.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <Windows.h>
+#include <tlhelp32.h>
+#include <tchar.h>
 #include <CasualLibrary.hpp>
+
+using namespace std;
+
+inline bool is_process_running(const wchar_t* processName)
+{
+    bool exists = false;
+    PROCESSENTRY32 entry;
+    entry.dwSize = sizeof(PROCESSENTRY32);
+
+    const HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+    if (Process32First(snapshot, &entry))
+        while (Process32Next(snapshot, &entry))
+            if (!_wcsicmp(entry.szExeFile, processName))
+                exists = true;
+
+    CloseHandle(snapshot);
+    return exists;
+}
 
 int main()
 {
+    #pragma region is Minecraft open check
+    const wstring name(L"Minecraft.Windows.exe");
+    const wchar_t* mc_process_name = name.c_str();
+
+    if (is_process_running(mc_process_name) == false)
+    {
+        MessageBox(NULL, _T("Minecraft Bedrock Edition isn't open! Please open Minecraft Bedrock Edition and then restart the program!"), _T("Error"), NULL);
+        return 0;
+    }
+    #pragma endregion
+
     Memory::External memory = Memory::External("Minecraft.Windows.exe", true);
 }
